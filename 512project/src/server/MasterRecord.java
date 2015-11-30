6ceb2f8d991ad8bc5eb60b889d482ccfdde33b04
ZhenyuWang <slowloriswzy@gmail.com>
2015-11-29 22:13:28 -0500

    M3_v0.2
    稍微改了一下MasterRecord的一个constructor, 虽然从来没用到过……

--- 512project/src/server/MasterRecord.java
+++ 512project/src/server/MasterRecord.java
@@ -3,47 +3,48 @@ package server;
 import java.io.Serializable;
 
 public class MasterRecord implements Serializable {
-    
+
     private boolean isAMaster;
     private int transactionID;
     private String fileA;
     private String fileB;
-    
+
     MasterRecord() {
         this.isAMaster = true;
         this.fileA = "/records/fileA.rm";
         this.fileB = "/records/fileB.rm";
     }
-    
+
     MasterRecord(int ID) {
         this.transactionID = ID;
+        this.isAMaster = true;
         this.fileA = "/records/fileA.rm";
         this.fileB = "/records/fileB.rm";
     }
-    
+
     MasterRecord(int ID, boolean A) {
         this.isAMaster = A;
         this.transactionID = ID;
         this.fileA = "/records/fileA.rm";
         this.fileB = "/records/fileB.rm";
     }
-    
+
     public void setPathA (String newPath) {
         this.fileA = newPath;
     }
-    
+
     public void setPathB (String newPaht) {
         this.fileA = newPath;
     }
-    
+
     public String getPathA () {
         return this.fileA;
     }
-    
+
     public String getPathB () {
         return this.fileB;
     }
-    
+
     public String getPointer () {
         if(this.isAMaster) {
             return "A";
@@ -52,11 +53,11 @@ public class MasterRecord implements Serializable {
             return "B";
         }
     }
-    
+
     public void togglePointer() {
         this.isAMaster = !this.isAMaster;
     }
-    
+
     public void setID(int ID) {
         this.transactionID = ID;
     }
